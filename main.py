c16cefaaef81d359be553617cf92167ed9b88118
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-06-01 18:16:25 +0200

    Glasses location smoothing works for 1 face

--- main.py
+++ main.py
@@ -41,23 +41,23 @@ class overlay:
     def draw(self, background):
         self.recentlyDrawn = True
 
-        delta = (self.desiredWarp[0] - self.currentWarp[0]) * 1/4
-        self.currentWarp[0]+= delta
+        delta = (self.desiredWarp[0] - self.currentWarp[0]) * 1 / 4
+        self.currentWarp[0] += delta
 
         delta = (self.desiredWarp[1] - self.currentWarp[1]) * 1 / 4
-        self.currentWarp[1]+= delta
+        self.currentWarp[1] += delta
 
-        delta = (self.desiredPosition[0] - self.currentPosition[0]) * 3/5
+        delta = (self.desiredPosition[0] - self.currentPosition[0]) * 3 / 5
         self.currentPosition[0] += int(delta)
 
-        delta = (self.desiredPosition[1] - self.currentPosition[1]) * 3/5
+        delta = (self.desiredPosition[1] - self.currentPosition[1]) * 3 / 5
         self.currentPosition[1] += int(delta)
 
         scaledGlasses = cv2.resize(self.img, None, fx=self.currentWarp[1], fy=self.currentWarp[1],
                                    interpolation=cv2.INTER_LINEAR)
         rotatedGlasses = imutils.rotate_bound(scaledGlasses, self.currentWarp[0])
         add_transparent_image(background, rotatedGlasses, int(self.currentPosition[1]), int(self.currentPosition[0]))
-        cv2.line(background,tuple(self.currentPosition),tuple(self.desiredPosition),(255,255,0),4)
+        cv2.line(background, tuple(self.currentPosition), tuple(self.desiredPosition), (255, 255, 0), 4)
 
     def isUnused(self):
         if self.framesOutOfImage < self.maxFramesOutOfImage:
@@ -88,7 +88,7 @@ while True:
     ret, img = camera.read()
     grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(grey, 1.3, 5)
-    # for each face
+    
     for i, (x, y, w, h) in enumerate(faces):
 
         # if overlay not existing then create
@@ -97,7 +97,7 @@ while True:
             glasses.append(overlay())
 
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
-        cv2.putText(img, i.__str__(), (x, y + 25), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 255))
+        cv2.putText(img, i.__str__(), (x, y + h), cv2.FONT_HERSHEY_PLAIN, 12, (255,0,255),5)
 
         face_grey = grey[y:y + h, x:x + w]
         face_color = img[y:y + h, x:x + w]
@@ -108,7 +108,6 @@ while True:
             if len(eyes) == 2:
                 oko = [ex + ew / 2, ey + eh / 2]
                 eyes_list.append(oko)
-                cv2.putText(face_color, i.__str__(), (int(oko[0]), int(oko[1])), cv2.FONT_HERSHEY_COMPLEX,1,(0,0,0))
 
         # jeżeli jedna para i różnica y jest poniżej 30.
         if len(eyes_list) == 2:
@@ -123,7 +122,7 @@ while True:
                 center_y = (eyes_list[0][1] + eyes_list[1][1]) / 2
                 newx = int(x + center_x)
                 newy = int(y + center_y)
-                glasses[i].setattr([newx, newy], [angle, scale])
+                glasses[i].setattr([newx,newy],[angle,scale])
 
                 cv2.circle(img, (int(x + eyes_list[0][0]), int(y + eyes_list[0][1])), 20, (0, 255, 0), 1)
                 cv2.circle(img, (int(x + eyes_list[1][0]), int(y + eyes_list[1][1])), 20, (0, 255, 0), 1)
