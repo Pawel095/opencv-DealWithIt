b19cb6604200585edc9bc894f1461d77bcffaab0
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-06-01 12:59:02 +0200

    AverageQueue is now functional

--- averageQueue.py
+++ averageQueue.py
@@ -1,14 +1,24 @@
+import statistics
+
+
 class averageQueue:
 
-    def __init__(self,maxSize=20):
-        self.__maxSize=20
-        self.__queue=list()
+    def __init__(self, maxSize=20):
+        self.__maxSize = maxSize
+        self.__queue = list()
 
-    def add(a):
-        """enqueues an item; if the items in the queue equal or exceed the maxSize then  the oldest is removed.
+    def add(self, a):
+        """enqueues an item; if the items in the queue are equal or exceed the maxSize then the oldest is removed.
         if a None is passed one item is removed."""
-        pass
+        if len(self.__queue) < self.__maxSize:
+            if a is not None:
+                self.__queue.append(a)
+            else:
+                self.__queue.pop(0)
+        else:
+            self.__queue.append(a)
+            self.__queue.pop(0)
 
     def get_avg(self):
         """:returns average of all values without removing any from the queue"""
-        pass
+        return statistics.mean(self.__queue)
--- test_averageQueue.py
+++ test_averageQueue.py
@@ -5,10 +5,14 @@ class TestaverageQueue(unittest.TestCase):
     def test_averageQueue(self):
         from averageQueue import averageQueue as aq;
         avg = aq(3)
-        aq.add(3)
-        aq.add(None)
-        aq.add(3)
-        self.assertAlmostEqual(avg.get_avg(), 2)
+        avg.add(3)
+        avg.add(None)
+        avg.add(3)
+        self.assertAlmostEqual(avg.get_avg(), 3)
 
-        aq.add(500)
-        self.assertAlmostEqual(aq.get_avg(), 168.67, 2)
+        avg=aq(3)
+        avg.add(3)
+        avg.add(3)
+        avg.add(3)
+        avg.add(500)
+        self.assertAlmostEqual(avg.get_avg(), 168.67, 2)
