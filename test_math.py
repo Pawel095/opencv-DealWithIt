c3bdd3809e174198402b2bef013d1790fef61cd3
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-06-01 15:28:26 +0200

    Added test and the approach function

--- test_math.py
+++ test_math.py
@@ -6,3 +6,13 @@ class TestMath(unittest.TestCase):
         from mathUtils import get_dist
         e=[[0,-1],[0,4]]
         self.assertAlmostEqual(get_dist(e), 5)
+
+    def test_approach(self):
+        from mathUtils import approach
+        val=0
+        desired=5
+        step=0.5
+        val=approach(val,desired,step)
+        self.assertAlmostEqual(val,2.5)
+        val = approach(val, desired, step)
+        self.assertAlmostEqual(val,3.75)
