aaaa642dafb763270c3f3fc0541bd38f88d33bad
Paweł 'Pawel095' Pankiewicz <pawelp6020@gmail.com>
2019-06-04 13:10:01 +0200

    FIxed weirdness when 2 faces are present

--- main.py
+++ main.py
@@ -2,7 +2,18 @@ import numpy as np
 import cv2
 import math
 import imutils
-from mathUtils import get_dist, approach
+from mathUtils import get_dist
+import statistics
+import random
+
+
+def exists(o):
+    try:
+        o
+    except NameError:
+        return False
+    else:
+        return True
 
 
 def add_transparent_image(bg, ov, x, y):
@@ -23,41 +34,26 @@ def add_transparent_image(bg, ov, x, y):
 
 
 class overlay:
-    maxFramesOutOfImage = 5
+    maxFramesOutOfImage = 20
     framesOutOfImage = 0
     recentlyDrawn = True
     #                  x, y
-    desiredPosition = [0, 0]
     currentPosition = [300, -500]
 
     #            rot, scl
-    desiredWarp = [0, 0.5]
-    currentWarp = [0, 1]
-
-    def __init__(self):
+    currentWarp = [0, 0.3]
+    def __init__(self,i):
         self.img = cv2.imread("glasses.png", cv2.IMREAD_UNCHANGED)
         self.img = cv2.resize(self.img, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
+        self.name = i
 
     def draw(self, background):
         self.recentlyDrawn = True
-
-        delta = (self.desiredWarp[0] - self.currentWarp[0]) * 1 /4
-        self.currentWarp[0] += delta
-
-        delta = (self.desiredWarp[1] - self.currentWarp[1]) * 1/4
-        self.currentWarp[1] += delta
-
-        delta = (self.desiredPosition[0] - self.currentPosition[0]) * 1/4
-        self.currentPosition[0] += int(delta)
-
-        delta = (self.desiredPosition[1] - self.currentPosition[1]) * 1/4
-        self.currentPosition[1] += int(delta)
 
         scaledGlasses = cv2.resize(self.img, None, fx=self.currentWarp[1], fy=self.currentWarp[1],
                                    interpolation=cv2.INTER_LINEAR)
         rotatedGlasses = imutils.rotate_bound(scaledGlasses, self.currentWarp[0])
         add_transparent_image(background, rotatedGlasses, int(self.currentPosition[1]), int(self.currentPosition[0]))
-        cv2.line(background, tuple(self.currentPosition), tuple(self.desiredPosition), (255, 255, 0), 4)
 
     def isUnused(self):
         if self.framesOutOfImage < self.maxFramesOutOfImage:
@@ -70,37 +66,44 @@ class overlay:
             return True
 
     def setx(self, a):
-        self.desiredPosition[0] = a
+        self.currentPosition[0]=statistics.mean((self.currentPosition[0],a))
 
     def sety(self, a):
-        self.desiredPosition[1] = a
+        self.currentPosition[1]=statistics.mean((self.currentPosition[1],a))
 
     def setAngle(self, a):
-        self.desiredWarp[0] = a
+        pass
 
     def setScale(self, a):
-        self.desiredWarp[1] = a
+        pass
 
     def setattr(self, pos, warp):
-        self.desiredPosition = pos
-        self.desiredWarp = warp
-        self.currentPosition = pos
-        self.currentWarp = warp
+        self.currentPosition[0] = statistics.mean((self.currentPosition[0], pos[0]))
+        self.currentPosition[1] = statistics.mean((self.currentPosition[1], pos[1]))
+
+        self.currentWarp=warp
 
 
-glasses = [overlay(), overlay(), overlay()]
+glasses = {}
 
 face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 eye_cascade = cv2.CascadeClassifier('haarcascade_eye.xml')
 camera = cv2.VideoCapture(0)
-img = cv2.imread("myUglyFace.jpg")
 while True:
     ret, img = camera.read()
     grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     faces = face_cascade.detectMultiScale(grey, 1.3, 5)
 
-    detectionResult = []
-    for i, (x, y, w, h) in enumerate(faces):
+    # todo create overlay classes
+    for i, face in enumerate(faces):
+        if i >= len(glasses):
+            print("creating "+i.__str__())
+            glasses[i]=overlay(i)
+
+    detectionResult = {}
+    index =0
+    for x, y, w, h in faces:
+
         face = {"face": (x, y, w, h)}
         cv2.rectangle(img, (x, y), (x + w, y + h), (255, 0, 0), 2)
         cv2.putText(img, i.__str__(), (x, y + h), cv2.FONT_HERSHEY_PLAIN, 12, (255, 0, 0), 5)
@@ -120,32 +123,57 @@ while True:
                 cv2.putText(face_img, '0', (ax, ay + ah), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 0), 2)
                 cv2.putText(face_img, '1', (bx, by + bh), cv2.FONT_HERSHEY_PLAIN, 5, (0, 0, 0), 2)
 
-        detectionResult.append(face)
+        detectionResult[index] = face
+        index+=1
 
-    calculationResult = []
-    for i, face in enumerate(detectionResult):
+    calculationResult = {}
+    for i, face in detectionResult.items():
         if "eyes" in face:
             scale = get_dist(face["eyes"]) / (225 * 0.7)
             tanTheta = (face["eyes"][0][1] - face["eyes"][1][1]) / (face["eyes"][0][0] - face["eyes"][1][0])
             angle = math.degrees(math.atan(tanTheta))
             targetx = int(face["face"][0] + (face["eyes"][0][0] + face["eyes"][1][0]) / 1.5)
             targety = int(face["face"][1] + (face["eyes"][0][1] + face["eyes"][1][1]) / 1.5)
-            calculationResult.append({"pos": [targetx, targety], "warp": [angle, scale]})
+            calculationResult[i] = {"pos": [targetx, targety], "warp": [angle, scale]}
         else:
             targetx = int(face["face"][0] + face["face"][2] / 2)
             targety = int(face["face"][1] + face["face"][3] / 2)
-            calculationResult.append({"pos": [targetx, targety]})
+            calculationResult[i] = {"pos": [targetx, targety]}
+    # return lista dictów z pos = [x,y] warp = [rot,scl]
 
-    for i, face in enumerate(calculationResult):
-        current = glasses[i]
-        # TODO tutaj pilnowanie co jest w dict a czego nie ma
+    #przypisz overlay do face na podstawie indexów
+
+
+
+    for i, face in calculationResult.items():
+        minDist = 19823912481
+        index = 0
+        for j,glass in glasses.items():
+            if get_dist((glass.currentPosition,face["pos"])) < minDist:
+                minDist = get_dist((glass.currentPosition,face["pos"]))
+                index = j
+                print(glasses[j].name)
+        current = glasses[index]
+        print("selecting",current.name,minDist)
+
+
         if "warp" in face:
-            current.setattr(face["pos"],face["warp"])
+            current.setattr(face["pos"], face["warp"])
         else:
             current.setx(face["pos"][0])
             current.sety(face["pos"][1])
 
-        current.draw(img)
+    if len(glasses.items()) >0 :
+        glasses[0].draw(img)
+    print("##########################################################")
+    # todo remove usless uverlay classes
+    removelist=[]
+    for i,g in glasses.items():
+        if g.isUnused():
+            print("killing "+i.__str__())
+            removelist.append(i)
+    for i in removelist:
+        glasses.pop(i)
 
     cv2.imshow("img", img)
     k = cv2.waitKey(30) & 0xff
--- mathUtils.py
+++ mathUtils.py
@@ -3,6 +3,7 @@ def get_dist(e):
     import math
     return math.sqrt(pow(e[0][0] - e[1][0], 2) + pow(e[0][1] - e[1][1], 2))
 
+
 def approach(val,desired,step):
     """slowly approaches a value. every execution: val will be closer to desired
     :argument val The value that will be modified approach
@@ -11,3 +12,11 @@ def approach(val,desired,step):
     delta=(desired-val)*step
     val+=delta
     return val
+
+
+def closeTo(pos1,pos2,delta):
+    """check if the positions are close together"""
+    if get_dist((pos1,pos2))<delta:
+        return True
+    else:
+        return False;
